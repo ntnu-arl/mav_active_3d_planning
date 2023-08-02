c0c28a6126ab7a2d6998a6429874455d0ab8d850
Samwise Gamgee <you@example.com>
2023-08-02 10:41:28 +0200

    fixed bug with going to the in_range node

--- active_3d_planning_core/src/planner/online_planner.cpp
+++ active_3d_planning_core/src/planner/online_planner.cpp
@@ -321,7 +321,7 @@ bool OnlinePlanner::requestNextTrajectory() {
     target_position_ = trajectory.back().position_W;
     target_yaw_ = trajectory.back().getYaw();
     back_tracker_->segmentIsExecuted(*current_segment_);
-  } while (current_segment_.get()->in_range > 0);
+  } while (current_segment_.get()->in_range > 1);
 
   // Visualize
   if (p_log_performance_) {
